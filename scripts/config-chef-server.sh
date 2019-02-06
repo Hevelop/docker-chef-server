b7bf258565c8378412aa937b59002eb8ce96e8dd
Alex Bordin <alex@hevelop.com>
2019-02-06 12:10:24 +0100

    Added config for external postgresql

--- scripts/config-chef-server.sh
+++ scripts/config-chef-server.sh
@@ -2,17 +2,7 @@
 
 set -x
 
-# Create chef-server.rb with variables
-if [[ -z $ENABLE_NON_SSL ]]; then
-    echo "nginx['enable_non_ssl']=false" >> /etc/opscode/chef-server.rb
-    # sed "100anode.default['private_chef']['nginx']['enable_non_ssl']=false" /opt/opscode/embedded/cookbooks/private-chef/recipes/nginx.rb > /opt/opscode/embedded/cookbooks/private-chef/recipes/nginx.rb
-else
-    echo "nginx['enable_non_ssl']=$ENABLE_NON_SSL" >> /etc/opscode/chef-server.rb
-    # sed "100anode.default['private_chef']['nginx']['enable_non_ssl']=$ENABLE_NON_SSL" /opt/opscode/embedded/cookbooks/private-chef/recipes/nginx.rb > /opt/opscode/embedded/cookbooks/private-chef/recipes/nginx.rb
-fi
-
-if [[ -z $SSL_PORT ]]; then
-  echo "nginx['ssl_port']=443" >> /etc/opscode/chef-server.rb
-else
-  echo "nginx['ssl_port']=$SSL_PORT" >> /etc/opscode/chef-server.rb
+# Copy chef-server.rb if not exist or empty
+if [[( ! -f /etc/opscode/chef-server.rb ) || (! -s /etc/opscode/chef-server.rb)]]; then
+    cp /chef-server.rb /etc/opscode/chef-server.rb
 fi
